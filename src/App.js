3bf6a1de5e40e441f0bd4d53d635739ca9e8957b
josephajith1212 <josephajith1212@gmail.com>
2022-03-02 13:18:40 -0500

    added empty pages and setup router for them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import './App.css';
+import Create from './pages/create/Create';
+import Dashboard from './pages/dashboard/Dashboard';
+import Login from './pages/login/Login';
 import Signup from './pages/signup/signup';
 
 function App() {
   return (
-    <div>
-      <Signup/>
+    <div className='App'>
+      <BrowserRouter>
+        <div className="container">
+          <Switch>
+            <Route exact path="/">
+              <Dashboard/>
+            </Route>
+            <Route path="/create">
+              <Create/>
+            </Route>
+            <Route path="/project">
+              <Create/>
+            </Route>
+            <Route path="/login">
+              <Login/>
+            </Route>
+            <Route path="/signup">
+              <Signup/>
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
