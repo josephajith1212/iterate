54d8a47a1dd350753c9e39f7e83f9c08ba31998d
Ajesh Wilson <9433713+jesh2@users.noreply.github.com>
2022-03-16 20:48:38 -0400

    Project List Completed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -11,7 +11,18 @@ export default function ProjectList({ projects }) {
       {projects.length === 0 && <p>No projects yet!</p>}
       {projects.map(project => (
         <Link to={`/projects/${project.id}`} key={project.id}>
-          
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          <div className="assigned-to">
+            <p><strong>Assigned to:</strong></p>
+            <ul>
+              {project.assignedUsersList.map(user => (
+                <li key={user.photoURL}>
+                  <Thumbnail src={user.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
         </Link>
       ))}
     </div>
