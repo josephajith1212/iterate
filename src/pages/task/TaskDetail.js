2c63663af7c04920c1b95234aab14a7defcae2e2
josephajith1212 <josephajith1212@gmail.com>
2022-04-13 18:51:37 -0400

    back to all tasks btn added

--- src/pages/task/TaskDetail.js
+++ src/pages/task/TaskDetail.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Thumbnail from "../../components/Thumbnail"
-import {useParams} from "react-router-dom"
+import {useHistory, useParams} from "react-router-dom"
 import {useDocument} from '../../hooks/useDocument'
 import TaskComments from './TaskComments'
 
 import './Task.css'
 
 export default function TaskDetail() {
+    let history = useHistory()
     const {id} = useParams()
     const {document, error} = useDocument('tasks', id)
 
@@ -16,31 +17,39 @@ export default function TaskDetail() {
     if (!document) {
         return <div className="loading">Loading...</div>
     }
-
+    const handleClick = (e) => {
+        history.push({
+          pathname: '/taskList',
+          state: {projectId : document.projectId, projectName : document.projectName}
+        })
+      }
     return (
-        <div className="task-detail">
-            <div>
-                <h2 className="page-title">{document.name}</h2>
-                <p>Created By - {document.createdBy.displayName}</p>
-                <p className="due-date">
-                    Task due by {document.dueDate.toDate().toDateString()}
-                </p>
-                <p className="description">
-                    {document.description}
-                </p>
-                <h4>Task assigned to:</h4>
-                <div className="assigned-users">
-                    {document.assignedUsersList.map(user => (
-                        <div key={user.id}>
-                            <Thumbnail src={user.photoURL} />
-                        </div>
-                    ))}
+        <>
+            <button className="btn backBtn" onClick={handleClick}>Back to All Tasks</button>
+            <div className="task-detail">
+                <div>
+                    <h2 className="page-title">{document.name}</h2>
+                    <p>Created By - {document.createdBy.displayName}</p>
+                    <p className="due-date">
+                        Task due by {document.dueDate.toDate().toDateString()}
+                    </p>
+                    <p className="description">
+                        {document.description}
+                    </p>
+                    <h4>Task assigned to:</h4>
+                    <div className="assigned-users">
+                        {document.assignedUsersList.map(user => (
+                            <div key={user.id}>
+                                <Thumbnail src={user.photoURL} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div>
+                    <TaskComments task={document} />
                 </div>
             </div>
-            <div>
-                <TaskComments task={document} />
-            </div>
-        </div>
+        </>
     )
 }
 //
